7ccd0f0338c2921d2845b9dfb09b157482a577b1
吕杰 <991102@nd.com>
2017-07-29 15:04:40 +0800

    完成主页面，  首页和问题列表的切换

--- react-boilerplate/src/question/QuestionMain.js
+++ react-boilerplate/src/question/QuestionMain.js
@@ -5,16 +5,57 @@
 import React, { Component } from 'react'
 import QuestionList from './QuestionList'
 import Header from './component/Header'
+import QuestionMainTop from './component/QuestionMainTop'
+import MyQuestionList from './MyQuestionList'
 
 export default class QuestionMain extends Component {
+  constructor() {
+    super()
+    this.state = {
+      isShowHomePage: true
+    }
+
+    this.homePageOnSelect = this.homePageOnSelect.bind(this)
+    this.myQuestionPageOnSelect = this.myQuestionPageOnSelect.bind(this)
+  }
+
   render() {
+    var selectPage = this.getSelectPage()
+    // this.getSelectPage()
     return (
       <div>
         <Header
           leftText='答!'
           rightText='提问' />
-        <QuestionList />
+        <QuestionMainTop
+          leftText='首页'
+          rightText='我的提问'
+          homePageOnSelect={this.homePageOnSelect}
+          myQuestionPageOnSelect={this.myQuestionPageOnSelect} />
+        {selectPage}
       </div>
     )
+  }
+
+  getSelectPage() {
+    if (this.state.isShowHomePage) {
+      return <QuestionList />
+    } else {
+      return <MyQuestionList />
+    }
+  }
+
+  homePageOnSelect(e) {
+    console.log('点击首页')
+    this.setState({
+      isShowHomePage: true
+    })
+  }
+
+  myQuestionPageOnSelect(e) {
+    console.log('点击我的提问')
+    this.setState({
+      isShowHomePage: false
+    })
   }
 }
---
+++ react-boilerplate/src/question/component/QuestionMainTop.js
@@ -0,0 +1,45 @@
+/**
+ * Created by Administrator on 2017/7/29 0029.
+ */
+import React, { Component } from 'react'
+import CSSModules from 'react-css-modules'
+import styles from '../../theme/styles/header.css'
+
+@CSSModules(styles, {allowMultiple: true})
+export default class QuestionMainTop extends Component {
+  static propTypes = {
+    leftText: React.PropTypes.string,
+    centerText: React.PropTypes.string,
+    rightText: React.PropTypes.string,
+    homePageOnSelect: React.PropTypes.func.isRequired,
+    myQuestionPageOnSelect: React.PropTypes.func.isRequired
+  }
+
+  static getDefaultProps = {
+    leftText: '返回',
+    centerText: '发布问题',
+    rightText: '提交'
+  }
+
+  leftClick(e) {
+    this.props.homePageOnSelect(e)
+  }
+
+  rightClick(e) {
+    this.props.myQuestionPageOnSelect(e)
+  }
+
+  render() {
+    return (
+      <div styleName='container'>
+        <button styleName='left-btn' onClick={(e) => {
+          this.leftClick(e)
+        }}>{this.props.leftText}</button>
+        <span>{this.props.centerText}</span>
+        <button styleName='right-btn' onClick={(e) => {
+          this.rightClick(e)
+        }}>{this.props.rightText}</button>
+      </div>
+    )
+  }
+}
