21473cbdde1109222dbc98e876400979f0296cbc
cjj <232249>
2017-07-29 18:52:35 +0800

    点赞取消赞

--- react-boilerplate/src/question/component/QuestionAnswerListItem.js
+++ react-boilerplate/src/question/component/QuestionAnswerListItem.js
@@ -11,6 +11,11 @@ export default class QuestionAnswerListItem extends Component {
     questionAnswerInfo: React.PropTypes.object
   }
 
+  constructor(props) {
+    super(props)
+    this.state = {like: this.props.questionAnswerInfo.answerUpCount, noLike: this.props.questionAnswerInfo.answerDownCount}
+  }
+
   static getDefaultProps = {
     questionAnswerInfo: {
       answerCreater: '',
@@ -21,14 +26,28 @@ export default class QuestionAnswerListItem extends Component {
     }
   }
 
+  like=() => {
+    this.setState({
+      like: this.state.like + 1
+    })
+    // 传送服务器
+  }
+
+  noLike=() => {
+    this.setState({
+      noLike: this.state.noLike > 1 ? this.state.noLike - 1 : 0
+    })
+    // 传送服务器
+  }
+
   render() {
     return (
       <div styleName='questionItem'>
         <span styleName='questionAnswer'>{this.props.questionAnswerInfo.answerCreater}</span>
         <span styleName='questionTime'>{this.props.questionAnswerInfo.answerCreateTime}</span>
         <div styleName='questionCount'>
-          <span style={{marginRight: 10}}>喜欢：{this.props.questionAnswerInfo.answerUpCount}</span>
-          <span>不喜欢:{this.props.questionAnswerInfo.answerDownCount}</span>
+          <span style={{marginRight: 10}} onClick={this.like}>喜欢：{ this.state.like > 999 ? '999+' : this.state.like }</span>
+          <span onClick={this.noLike}>不喜欢:{ this.state.noLike > 999 ? '999+' : this.state.noLike }</span>
         </div>
         <p styleName='questionContent'>{this.props.questionAnswerInfo.answerContent}</p>
       </div>
